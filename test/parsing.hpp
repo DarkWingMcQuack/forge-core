b13be475b4e0a7498a0a0054ae30b3df51475a94
DarkWingMcQuack <np-hardass@protonmail.com>
2018-10-14 14:14:23 +0200

    moved parsing helper functions into own header

---
+++ test/parsing.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "resource_path.hpp"
+#include <fstream>
+#include <iterator>
+#include <json/json.h>
+#include <json/reader.h>
+#include <json/value.h>
+#include <json/writer.h>
+
+
+auto readFile(const std::string& path)
+    -> std::string
+{
+    std::ifstream t{resource_path + "/" + path};
+    std::string str{std::istreambuf_iterator<char>(t),
+                    std::istreambuf_iterator<char>()};
+
+    return str;
+}
+
+auto parseString(const std::string& str)
+    -> Json::Value
+{
+    Json::CharReaderBuilder builder;
+    Json::CharReader* reader = builder.newCharReader();
+
+    Json::Value json;
+    std::string errors;
+
+    bool parsingSuccessful = reader->parse(
+        str.c_str(),
+        str.c_str() + str.size(),
+        &json,
+        &errors);
+    delete reader;
+
+    return json;
+}
--- test/transaction_tests.cpp
+++ test/transaction_tests.cpp
@@ -1,41 +1,8 @@
-#include "resource_path.hpp"
+#include "parsing.hpp"
 #include <core/Transaction.hpp>
-#include <fstream>
 #include <gtest/gtest.h>
-#include <iterator>
-#include <json/json.h>
-#include <json/reader.h>
 #include <json/value.h>
-#include <json/writer.h>
 
-auto readFile(const std::string& path)
-    -> std::string
-{
-    std::ifstream t{resource_path + "/" + path};
-    std::string str{std::istreambuf_iterator<char>(t),
-                    std::istreambuf_iterator<char>()};
-
-    return str;
-}
-
-auto parseString(const std::string& str)
-    -> Json::Value
-{
-    Json::CharReaderBuilder builder;
-    Json::CharReader* reader = builder.newCharReader();
-
-    Json::Value json;
-    std::string errors;
-
-    bool parsingSuccessful = reader->parse(
-        str.c_str(),
-        str.c_str() + str.size(),
-        &json,
-        &errors);
-    delete reader;
-
-    return json;
-}
 
 TEST(TransactionTest, TxInParsingValid)
 {
