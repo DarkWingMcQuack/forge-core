64fbe196a221515ee15c456f80751e1ad8ef3238
DarkWingMcQuack <np-hardass@protonmail.com>
2018-10-27 19:42:34 +0200

    fixed the processBlock method

--- src/lookup/LookupManager.cpp
+++ src/lookup/LookupManager.cpp
@@ -12,6 +12,7 @@ using buddy::lookup::EntryLookup;
 using buddy::lookup::LookupError;
 using buddy::core::EntryKey;
 using buddy::core::EntryValue;
+using buddy::core::Operation;
 using buddy::util::Opt;
 using buddy::util::Result;
 using buddy::util::traverse;
@@ -93,14 +94,43 @@ auto LookupManager::lookupOwner(const core::EntryKey& key) const
 auto LookupManager::processBlock(core::Block&& block)
     -> util::Result<void, ManagerError>
 {
+    auto block_height = block.getHeight();
+
+    //traverse all txids to transactions
     return traverse(std::move(block.getTxids()),
                     [this](auto&& txid) {
                         return daemon_->getTransaction(std::move(txid))
                             .mapError([](auto&& error) {
                                 return ManagerError{std::move(error)};
-                            })
-                            .flatMap([this](auto&& tx) {
-                                return processTransaction(std::move(tx));
                             });
-                    });
+                    })
+        .flatMap([&](auto&& txs)
+                     -> Result<void, ManagerError> {
+            //vector of all buddy ops in the block
+            std::vector<Operation> ops;
+            //exract all buddy ops from the txs
+            for(auto&& tx : txs) {
+                auto op_res = parseTransactionToEntry(std::move(tx),
+                                                      block_height,
+                                                      daemon_);
+                //if we dont get an opt, but an error, we return it
+                if(!op_res) {
+                    return ManagerError{std::move(op_res.getError())};
+                }
+
+                //check if the operation was parsed, and if
+                //we add it to the std::vector<Operation> vec
+                if(auto op_opt = std::move(op_res.getValue());
+                   op_opt) {
+                    ops.push_back(std::move(op_opt.getValue()));
+                }
+            }
+
+            //execture the operations by the lookup
+            return lookup_
+                .executeOperations(std::move(ops))
+                .mapError([](auto&& error) {
+                    return ManagerError{std::move(error)};
+                });
+        });
 }
