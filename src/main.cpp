e4909665a9bbdbbb59ad8229a7640f7f9bcf20e6
DarkWingMcQuack <np-hardass@protonmail.com>
2018-11-05 23:59:23 +0100

    deleted unused parse_args function

--- src/main.cpp
+++ src/main.cpp
@@ -21,51 +21,6 @@ using buddy::env::initConsoleLogger;
 using buddy::env::initFileLogger;
 using buddy::env::parseOptions;
 
-auto parse_args(int argc, char* argv[])
-    -> std::tuple<std::string,
-                  std::string,
-                  std::string,
-                  std::string,
-                  std::size_t>
-{
-    cxxopts::Options options("cppBUDDY", "Implementation of the BUDDY protocol");
-
-    // clang-format off
-    options.add_options()
-        ("help", "Print help and exit.")
-        ("l,logpath", "logpath",cxxopts::value<std::string>()->default_value(""))
-        ("u,user", "daemon RPC user",cxxopts::value<std::string>())
-        ("x,password", "daemon RPC password",cxxopts::value<std::string>())
-        ("h,host", "hostname of the daemon",cxxopts::value<std::string>())
-        ("p,port", "port of the RPC daemon",cxxopts::value<std::size_t>());
-    // clang-format on
-
-    try {
-        auto result = options.parse(argc, argv);
-
-        if(result.count("help")) {
-            fmt::print("{}\n", options.help());
-            exit(0);
-        }
-
-        auto logfile = result["logpath"].as<std::string>();
-        auto user = result["user"].as<std::string>();
-        auto password = result["password"].as<std::string>();
-        auto host = result["host"].as<std::string>();
-        auto port = result["port"].as<std::size_t>();
-
-        return {std::move(logfile),
-                std::move(user),
-                std::move(password),
-                std::move(host),
-                port};
-
-    } catch(const std::exception& e) {
-        fmt::print("{}\n", e.what());
-        std::exit(-1);
-    }
-}
-
 auto main(int argc, char* argv[]) -> int
 {
     using namespace std::chrono_literals;
