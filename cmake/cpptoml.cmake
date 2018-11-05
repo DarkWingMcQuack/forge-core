f21b2b611b700e3247f20f90f3acf0d42c233f24
DarkWingMcQuack <np-hardass@protonmail.com>
2018-11-05 20:19:32 +0100

    use

---
+++ cmake/cpptoml.cmake
@@ -0,0 +1,27 @@
+include(ExternalProject)
+
+set(CMAKE_ARGS
+  -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+  -DCMAKE_BUILD_TYPE=Release
+  -DCPPTOML_BUILD_EXAMPLES=OFF
+  -DENABLE_LIBCXX=OFF)
+
+
+ExternalProject_Add(cpptoml-project
+  PREFIX deps/cpptoml
+  DOWNLOAD_NAME cpptoml-0.1.1.tar.gz
+  DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/downloads
+  URL https://github.com/skystrife/cpptoml/archive/v0.1.1.tar.gz
+  PATCH_COMMAND cmake -E make_directory <SOURCE_DIR>/win32-deps/include
+  CMAKE_ARGS ${CMAKE_ARGS}
+  # Overwtire build and install commands to force Release build on MSVC.
+  BUILD_COMMAND cmake --build <BINARY_DIR> --config Release
+  INSTALL_COMMAND cmake --build <BINARY_DIR> --config Release --target install
+  )
+
+
+ExternalProject_Get_Property(utilxx-project INSTALL_DIR)
+set(UTILXX_INCLUDE_DIR ${INSTALL_DIR}/include)
+
+unset(INSTALL_DIR)
+unset(CMAKE_ARGS)
