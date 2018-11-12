5c763a068bf0366607b6424b28be57cf820fe49d
DarkWingMcQuack <np-hardass@protonmail.com>
2018-11-12 21:39:16 +0100

    added more functions to ReadWriteDaemonBase and implemented writeTyToBlockchain

--- include/daemon/ReadWriteDaemonBase.hpp
+++ include/daemon/ReadWriteDaemonBase.hpp
@@ -2,7 +2,7 @@
 
 #include <core/Operation.hpp>
 #include <daemon/DaemonError.hpp>
-#include <daemon/ReadWriteDaemonBase.hpp>
+#include <daemon/ReadOnlyDaemonBase.hpp>
 #include <utilxx/Result.hpp>
 
 namespace buddy::daemon {
@@ -14,10 +14,26 @@ public:
 
     virtual auto writeTxToBlockchain(std::string&& txid_input,
                                      std::vector<std::byte>&& metadata,
-                                     std::size_t op_return_value,
+                                     std::size_t burn_value,
                                      std::vector<
                                          std::pair<std::string,
-                                                   std::size_t>>&& outputs)
+                                                   std::size_t>>&& outputs) const
+        -> utilxx::Result<void, DaemonError>;
+
+    virtual auto generateRawTx(std::string&& input_txid,
+                               std::vector<std::byte>&& metadata,
+                               std::size_t burn_value,
+                               std::vector<
+                                   std::pair<std::string,
+                                             std::size_t>>&& outputs) const
+        -> utilxx::Result<std::vector<std::byte>,
+                          DaemonError> = 0;
+
+    virtual auto signRawTx(std::vector<std::byte>&& tx) const
+        -> utilxx::Result<std::vector<std::byte>,
+                          DaemonError> = 0;
+
+    virtual auto sendRawTx(std::vector<std::byte>&& tx) const
         -> utilxx::Result<void, DaemonError> = 0;
 };
 
---
+++ src/daemon/ReadWriteDaemonBase.cpp
@@ -0,0 +1,29 @@
+#include <core/Operation.hpp>
+#include <daemon/DaemonError.hpp>
+#include <daemon/ReadOnlyDaemonBase.hpp>
+#include <daemon/ReadWriteDaemonBase.hpp>
+#include <utilxx/Result.hpp>
+
+using buddy::daemon::ReadWriteDaemonBase;
+
+
+
+auto ReadWriteDaemonBase::writeTxToBlockchain(std::string&& txid_input,
+                                              std::vector<std::byte>&& metadata,
+                                              std::size_t burn_value,
+                                              std::vector<
+                                                  std::pair<std::string,
+                                                            std::size_t>>&& outputs) const
+    -> utilxx::Result<void, DaemonError>
+{
+    return generateRawTx(std::move(txid_input),
+                         std::move(metadata),
+                         burn_value,
+                         std::move(outputs))
+        .flatMap([this](auto&& raw_tx) {
+            return signRawTx(std::move(raw_tx));
+        })
+        .flatMap([this](auto&& signed_tx) {
+            return sendRawTx(std::move(signed_tx));
+        });
+}
