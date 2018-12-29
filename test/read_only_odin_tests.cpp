a364468ff0e36b32fc69503786b3cbf0b2564026
DarkWingMcQuack <np-hardass@protonmail.com>
2018-12-29 21:32:36 +0100

    added tests for getUnspent

--- test/read_only_odin_tests.cpp
+++ test/read_only_odin_tests.cpp
@@ -1,5 +1,6 @@
 #include "parsing.hpp"
 #include <daemon/odin/ReadOnlyOdinDaemon.hpp>
+#include <fmt/core.h>
 #include <gtest/gtest.h>
 #include <json/value.h>
 
@@ -39,7 +40,6 @@ TEST(ReadOnlyOdinDaemonTest, processGetBlockHashInvalid)
     ASSERT_TRUE(res_4.hasError());
 }
 
-
 TEST(ReadOnlyOdinDaemonTest, processGetBlockCountValid)
 {
     auto json_str1 = readFile("block_count_valid1.json");
@@ -74,4 +74,29 @@ TEST(ReadOnlyOdinDaemonTest, processGetBlockCountInvalid)
 
     auto res_4 = buddy::daemon::odin::processGetBlockCountResponse(std::move(json4), {});
     ASSERT_TRUE(res_4.hasError());
+}
+
+TEST(ReadOnlyOdinDaemonTest, processGetUnspentValid)
+{
+    auto json_str1 = readFile("unspent_valid1.json");
+    auto json1 = parseString(json_str1);
+
+    auto res_1 = buddy::daemon::odin::processGetUnspentResponse(std::move(json1), {});
+    ASSERT_TRUE(res_1.hasValue());
+    EXPECT_EQ(res_1.getValue().size(), 1);
+
+    auto json_str2 = readFile("unspent_valid2.json");
+    auto json2 = parseString(json_str2);
+
+    auto res_2 = buddy::daemon::odin::processGetUnspentResponse(std::move(json2), {});
+    ASSERT_TRUE(res_2.hasValue());
+    EXPECT_EQ(res_2.getValue().size(), 3);
+
+
+    auto json_str3 = readFile("unspent_valid3.json");
+    auto json3 = parseString(json_str3);
+
+    auto res_3 = buddy::daemon::odin::processGetUnspentResponse(std::move(json3), {});
+    ASSERT_TRUE(res_3.hasValue());
+    EXPECT_EQ(res_3.getValue().size(), 0);
 }
