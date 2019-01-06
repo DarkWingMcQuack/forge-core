edd72c2505ccbdf8e7b83ee64293d4e1d4b6be1b
DarkWingMcQuack <np-hardass@protonmail.com>
2019-01-06 02:16:58 +0100

    addded first implementation of read only wallet

---
+++ include/wallet/ReadOnlyWallet.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <lookup/LookupManager.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace buddy::wallet {
+
+class ReadOnlyWallet
+{
+public:
+    auto update()
+        -> void;
+
+    auto addWatchOnlyAddress(std::string&& adr)
+        -> void;
+
+    auto deleteWatchOnlyAddress(const std::string& adr)
+        -> void;
+
+    auto addNewOwnedAddress(std::string&& adr)
+        -> void;
+
+    auto getOwnedEntrys() const
+        -> std::vector<core::Entry>;
+
+    auto getWatchOnlyEntrys() const
+        -> std::vector<core::Entry>;
+
+    auto getAllWatchedEntrys() const
+        -> std::vector<core::Entry>;
+
+private:
+    std::vector<std::string> owned_addresses_;
+    std::vector<std::string> watched_addresses_;
+    std::unique_ptr<lookup::LookupManager> lookup_;
+};
+
+} // namespace buddy::wallet
---
+++ src/wallet/ReadOnlyWallet.cpp
@@ -0,0 +1,75 @@
+#include <lookup/LookupManager.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+#include <wallet/ReadOnlyWallet.hpp>
+
+using buddy::lookup::LookupManager;
+using buddy::lookup::LookupError;
+using buddy::wallet::ReadOnlyWallet;
+using buddy::core::Entry;
+
+
+
+auto ReadOnlyWallet::addWatchOnlyAddress(std::string&& adr)
+    -> void
+{
+    watched_addresses_.push_back(std::move(adr));
+}
+
+auto ReadOnlyWallet::deleteWatchOnlyAddress(const std::string& adr)
+    -> void
+{
+    watched_addresses_.erase(
+        std::remove(std::begin(watched_addresses_),
+                    std::end(watched_addresses_),
+                    adr),
+        std::end(watched_addresses_));
+}
+
+auto ReadOnlyWallet::addNewOwnedAddress(std::string&& adr)
+    -> void
+{
+    owned_addresses_.push_back(std::move(adr));
+}
+
+auto ReadOnlyWallet::getOwnedEntrys() const
+    -> std::vector<Entry>
+{
+    std::vector<Entry> ret_vec;
+    for(auto&& addr : owned_addresses_) {
+        auto new_entrys = lookup_->getEntrysOfOwner(addr);
+        ret_vec.insert(std::end(ret_vec),
+                       std::make_move_iterator(std::begin(new_entrys)),
+                       std::make_move_iterator(std::end(new_entrys)));
+    }
+
+    return ret_vec;
+}
+
+auto ReadOnlyWallet::getWatchOnlyEntrys() const
+    -> std::vector<Entry>
+{
+    std::vector<Entry> ret_vec;
+    for(auto&& addr : watched_addresses_) {
+        auto new_entrys = lookup_->getEntrysOfOwner(addr);
+        ret_vec.insert(std::end(ret_vec),
+                       std::make_move_iterator(std::begin(new_entrys)),
+                       std::make_move_iterator(std::end(new_entrys)));
+    }
+
+    return ret_vec;
+}
+
+auto ReadOnlyWallet::getAllWatchedEntrys() const
+    -> std::vector<Entry>
+{
+    auto owned_entrys = getOwnedEntrys();
+    auto watched_entrys = getWatchOnlyEntrys();
+
+    owned_entrys.insert(std::begin(owned_entrys),
+                        std::make_move_iterator(std::begin(watched_entrys)),
+                        std::make_move_iterator(std::end(watched_entrys)));
+
+    return owned_entrys;
+}
