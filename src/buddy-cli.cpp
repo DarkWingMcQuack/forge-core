196161b45149cc315f507c3cb76bfd60b0abc8fb
DarkWingMcQuack <np-hardass@protonmail.com>
2019-01-16 00:30:09 +0100

    added first element of readwrite rpc call

--- src/buddy-cli.cpp
+++ src/buddy-cli.cpp
@@ -8,6 +8,10 @@
 #include <fmt/ostream.h>
 #include <fmt/ranges.h>
 #include <g3log/g3log.hpp>
+#include <json/json.h>
+#include <json/reader.h>
+#include <json/value.h>
+#include <json/writer.h>
 #include <jsonrpccpp/client/connectors/httpclient.h>
 #include <rpc/readwritewalletstubclient.h>
 #include <thread>
@@ -28,16 +32,112 @@ using jsonrpc::JSONRPC_CLIENT_V2;
 using jsonrpc::JsonRpcException;
 
 
-std::string key;
-bool is_string;
-std::string owner;
-Json::Value entry_value;
-int burn_value;
+std::string key = "";
+bool is_string = false;
+std::string owner = "";
+std::string entry_value_str =
+    "{\"type\" : \"none\","
+    "\"value\":\"\"}";
+Json::Value entry_value = Json::nullValue;
+int burn_value = 0;
 
 
 Json::Value response;
 
-auto addLookupOnlySubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
+static auto parseIntoJson(const std::string& str)
+{
+    Json::CharReaderBuilder builder;
+    Json::CharReader* reader = builder.newCharReader();
+
+    Json::Value json;
+    std::string errors;
+
+    bool parsingSuccessful = reader->parse(
+        str.c_str(),
+        str.c_str() + str.size(),
+        &json,
+        &errors);
+    delete reader;
+
+    return json;
+}
+
+static auto checkAndTransformValueString(const std::string& str)
+    -> Json::Value
+{
+    auto json = parseIntoJson(str);
+
+    if(!json.isMember("type")
+       || !json["type"].isString()) {
+        fmt::print("given json value {}, is invalid, the json object needs a string field with name \"type\"\n",
+                   json.toStyledString());
+        std::exit(0);
+    }
+
+    const auto& type = json["type"].asString();
+
+    if(type != "ipv4"
+       && type != "ipv6"
+       && type != "bytearray"
+       && type != "none") {
+
+        fmt::print(
+            "in value json object only the type \"ipv4\", \"ipv6\", "
+            "\"bytearray\" or \"none\" are allowed, currently given {}\n",
+            type);
+        std::exit(0);
+    }
+
+
+    if(type == "ipv4") {
+        const auto& value = json["value"].asString();
+        if(value.size() != 8) {
+            fmt::print("given ipv4 value has invalid length of {}, but should have 8\n",
+                       value.size());
+            std::exit(0);
+        }
+
+        if(!stringToByteVec(value)) {
+            fmt::print("given value {} cannot be converted to byte vector\n",
+                       value);
+            std::exit(0);
+        }
+
+    } else if(type == "ipv6") {
+        const auto& value = json["value"].asString();
+        if(value.size() != 32) {
+            fmt::print("given ipv6 value has invalid length of {}, but should have 8\n",
+                       value.size());
+            std::exit(0);
+        }
+
+        if(!stringToByteVec(value)) {
+            fmt::print("given value {} cannot be converted to byte vector\n",
+                       value);
+            std::exit(0);
+        }
+
+    } else if(type == "bytevec") {
+        const auto& value = json["value"].asString();
+        if(!stringToByteVec(value)) {
+            fmt::print("given value {} cannot be converted to byte vector",
+                       value);
+            std::exit(0);
+        }
+    } else if(type == "none"
+              && json.isMember("value")) {
+
+        if(!json["value"].isString()
+           || !json["value"].asString().empty()) {
+            fmt::print("since type of value is \"none\", given value {} will be ignored\n",
+                       json["value"].toStyledString());
+        }
+    }
+
+    return json;
+}
+
+static auto addLookupOnlySubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
 {
     app.add_subcommand("shutdown",
                        "stops the buddyd server")
@@ -131,7 +231,7 @@ auto addLookupOnlySubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
         ->required();
 }
 
-auto addReadOnlySubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
+static auto addReadOnlySubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
 {
     app.add_subcommand("addwatchonlyaddress",
                        "adds a new address to watch")
@@ -188,13 +288,13 @@ auto addReadOnlySubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
         });
 }
 
-auto addReadWriteSubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
+static auto addReadWriteSubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
 {
-
     auto createnewentry_opt =
         app.add_subcommand("createnewentry",
                            "creates a new entry")
             ->callback([&] {
+                entry_value = checkAndTransformValueString(entry_value_str);
                 response = client.createnewentry(owner, burn_value, is_string, key, entry_value);
             });
 
@@ -206,6 +306,31 @@ auto addReadWriteSubcommands(CLI::App& app, ReadWriteWalletStubClient& client)
                      key,
                      "the key of which the value will be looked up")
         ->required();
+
+    createnewentry_opt
+        ->add_flag("--isstring",
+                   is_string,
+                   "if set, the given key will be interpreted as string and not as byte vector");
+
+    createnewentry_opt
+        ->add_option("--value",
+                     entry_value_str,
+                     "value the entry holds, expected to be a json object\n"
+                     "with valid \"type\" and \"value\" fields.\n"
+                     "If not given, the entry will hold \"none\" as value");
+
+    createnewentry_opt
+        ->add_option("--burn-value",
+                     burn_value,
+                     "number of coins which will be burned")
+        ->required();
+
+    createnewentry_opt
+        ->add_option("--address",
+                     owner,
+                     "optional address which will be used as owner of the new entry,\n"
+                     "be sure the used wallet ownes the private key to this address.\n"
+                     "If not given, then a new address will be generated");
 }
 
 
@@ -222,8 +347,6 @@ auto main(int argc, char* argv[]) -> int
     HttpClient httpclient("http://localhost:" + std::to_string(port));
     ReadWriteWalletStubClient client{httpclient, JSONRPC_CLIENT_V2};
 
-    Json::Value response;
-
     CLI::App app{"buddy-cli is a tool to easily communicate with a buddyd server via CLI"};
 
     app.set_help_all_flag("--help-all",
@@ -231,8 +354,7 @@ auto main(int argc, char* argv[]) -> int
 
     addLookupOnlySubcommands(app, client);
     addReadOnlySubcommands(app, client);
-
-
+    addReadWriteSubcommands(app, client);
 
 
     try {
