2cbe2666af73ba319abe2b821a6c4cdfba896ab2
DarkWingMcQuack <np-hardass@protonmail.com>
2019-01-25 17:35:15 +0100

    workdir can now be specified in forge-cli

--- src/forge-cli.cpp
+++ src/forge-cli.cpp
@@ -41,17 +41,20 @@ auto main(int argc, char* argv[]) -> int
 {
     using namespace std::string_literals;
 
-    static const auto default_forge_dir = getenv("HOME") + "/.forge"s;
+    static auto default_forge_dir = getenv("HOME") + "/.forge"s;
 
-    auto params = parseConfigFile(default_forge_dir);
-    auto port = params.getRpcPort();
-    HttpClient httpclient("http://localhost:" + std::to_string(port));
-    ReadWriteWalletStubClient client{httpclient, JSONRPC_CLIENT_V2};
 
     CLI::App app{"forge-cli is a tool to easily communicate with a forged server via CLI"};
 
     app.set_help_all_flag("--help-all",
                           "Show all help");
+
+    app.add_option("--workdir", default_forge_dir);
+
+    auto params = parseConfigFile(default_forge_dir);
+    auto port = params.getRpcPort();
+    HttpClient httpclient("http://localhost:" + std::to_string(port));
+    ReadWriteWalletStubClient client{httpclient, JSONRPC_CLIENT_V2};
 
     app.require_subcommand();
 
