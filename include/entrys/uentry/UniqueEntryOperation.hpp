c073726562463112dd30381ff2ece04ff3a3e9aa
lukas <np-hardass@protonmail.com>
2019-08-27 15:37:04 +0200

    added UniqueEntryOperation

---
+++ include/entrys/uentry/UniqueEntryOperation.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <array>
+#include <core/Transaction.hpp>
+#include <cstddef>
+#include <daemon/ReadOnlyDaemonBase.hpp>
+#include <entrys/uentry/UniqueEntry.hpp>
+#include <entrys/uentry/UniqueEntryCreationOp.hpp>
+#include <entrys/uentry/UniqueEntryDeletionOp.hpp>
+#include <entrys/uentry/UniqueEntryOwnershipTransferOp.hpp>
+#include <entrys/uentry/UniqueEntryRenewalOp.hpp>
+#include <memory>
+#include <vector>
+
+
+namespace forge::core {
+
+// clang-format off
+constexpr inline auto UNIQUE_ENTRY_CREATION_FLAG           = static_cast<std::byte>(0b00000001);
+constexpr inline auto UNIQUE_ENTRY_RENEWAL_FLAG            = static_cast<std::byte>(0b00000010);
+constexpr inline auto UNIQUE_ENTRY_OWNERSHIP_TRANSFER_FLAG = static_cast<std::byte>(0b00000100);
+constexpr inline auto UNIQUE_ENTRY_DELETION_FLAG           = static_cast<std::byte>(0b00001000);
+// clang-format on
+
+using UniqueEntryOperation = std::variant<UniqueEntryCreationOp,
+                                          UniqueEntryRenewalOp,
+                                          UniqueEntryOwnershipTransferOp,
+                                          UniqueEntryDeletionOp>;
+
+//extracts the EntryKey from a given operaton
+auto getEntryKey(const UniqueEntryOperation&)
+    -> const EntryKey&;
+auto getEntryKey(UniqueEntryOperation &&)
+    -> EntryKey;
+
+//extracts the UniqueEntry from a given operaton
+auto getUniqueEntry(const UniqueEntryOperation&)
+    -> const UniqueEntry&;
+auto getUniqueEntry(UniqueEntryOperation &&)
+    -> UniqueEntry;
+
+//extracts owner of the UniqueEntry from a given operaton
+auto getOwner(const UniqueEntryOperation&)
+    -> const std::string&;
+auto getOwner(UniqueEntryOperation &&)
+    -> std::string;
+
+//extracts burn value with which the given operation was
+//burned onto the chain
+auto getValue(const UniqueEntryOperation&)
+    -> const std::int64_t;
+
+//extracts the operation flag which specifys which operation it is
+auto extractOperationFlag(const UniqueEntryOperation&)
+    -> std::byte;
+
+//checks the metadata of a transaction and parses it into
+//an UniqueEntryOperation if it holds the needed information
+//and the metadata has the needed formating
+auto parseTransactionToUniqueEntry(core::Transaction&& tx,
+                                   std::int64_t block,
+                                   const daemon::ReadOnlyDaemonBase* daemon)
+    -> utilxx::Result<utilxx::Opt<UniqueEntryOperation>, daemon::DaemonError>;
+
+//parses given metadata and constructs a UniqueEntryOperation from
+//the given information if possible
+auto parseMetadata(const std::vector<std::byte>& metadata,
+                   std::int64_t block,
+                   std::string&& owner,
+                   std::int64_t value,
+                   utilxx::Opt<std::string>&& new_owner = std::nullopt)
+    -> utilxx::Opt<UniqueEntryOperation>;
+
+//given a UniqueEntryOperation, this function builds a matching
+//metadata which can be used to write it on the blockchain
+auto toMetadata(const UniqueEntryOperation& op)
+    -> std::vector<std::byte>;
+
+} // namespace forge::core
---
+++ src/entrys/uentry/UniqueEntryOperation.cpp
@@ -0,0 +1,273 @@
+#include <core/FlagIndexes.hpp>
+#include <core/Transaction.hpp>
+#include <cstddef>
+#include <daemon/ReadOnlyDaemonBase.hpp>
+#include <entrys/uentry/UniqueEntry.hpp>
+#include <entrys/uentry/UniqueEntryOperation.hpp>
+#include <fmt/core.h>
+#include <g3log/g3log.hpp>
+#include <utilxx/Opt.hpp>
+#include <utilxx/Overload.hpp>
+#include <utilxx/Result.hpp>
+#include <vector>
+
+using forge::core::UniqueEntry;
+using utilxx::Result;
+using utilxx::overload;
+using utilxx::Opt;
+using forge::core::Transaction;
+using forge::daemon::ReadOnlyDaemonBase;
+using forge::daemon::DaemonError;
+using forge::core::parseUniqueEntry;
+using forge::core::FORGE_IDENTIFIER_MASK;
+
+auto forge::core::getEntryKey(const UniqueEntryOperation& operation)
+    -> const EntryKey&
+{
+    return std::visit(
+        [](const auto& op)
+            -> const EntryKey& {
+            return op.getEntryKey();
+        },
+        operation);
+}
+
+auto forge::core::getEntryKey(UniqueEntryOperation&& operation)
+    -> EntryKey
+{
+    return std::visit(
+        [](auto&& op) {
+            return std::move(op.getEntryKey());
+        },
+        operation);
+}
+
+auto forge::core::getUniqueEntry(const UniqueEntryOperation& operation)
+    -> const UniqueEntry&
+{
+    return std::visit(
+        [](const auto& op)
+            -> const UniqueEntry& {
+            return op.getUniqueEntry();
+        },
+        operation);
+}
+
+auto forge::core::getUniqueEntry(UniqueEntryOperation&& operation)
+    -> UniqueEntry
+{
+    return std::visit(
+        [](auto&& op) {
+            return std::move(op.getUniqueEntry());
+        },
+        operation);
+}
+
+auto forge::core::getOwner(const UniqueEntryOperation& operation)
+    -> const std::string&
+{
+    return std::visit(
+        [](const auto& op)
+            -> const std::string& {
+            return op.getOwner();
+        },
+        operation);
+}
+
+auto forge::core::getOwner(UniqueEntryOperation&& operation)
+    -> std::string
+{
+    return std::visit(
+        [](auto&& op) {
+            return std::move(op.getOwner());
+        },
+        operation);
+}
+
+auto forge::core::getValue(const UniqueEntryOperation& operation)
+    -> const std::int64_t
+{
+    return std::visit(
+        [](auto&& op) {
+            return op.getValue();
+        },
+        operation);
+}
+
+auto forge::core::extractOperationFlag(const UniqueEntryOperation& operation)
+    -> std::byte
+{
+    constexpr static auto flag_extractor =
+        overload{
+            [](const UniqueEntryCreationOp&) { return UNIQUE_ENTRY_CREATION_FLAG; },
+            [](const UniqueEntryRenewalOp&) { return UNIQUE_ENTRY_RENEWAL_FLAG; },
+            [](const UniqueEntryOwnershipTransferOp&) { return UNIQUE_ENTRY_OWNERSHIP_TRANSFER_FLAG; },
+            [](const UniqueEntryDeletionOp&) { return UNIQUE_ENTRY_DELETION_FLAG; }};
+
+    return std::visit(flag_extractor,
+                      operation);
+}
+
+
+auto forge::core::parseMetadata(const std::vector<std::byte>& metadata,
+                                std::int64_t block,
+                                std::string&& owner,
+                                std::int64_t value,
+                                utilxx::Opt<std::string>&& new_owner_opt)
+    -> Opt<UniqueEntryOperation>
+{
+    if(metadata.size() < 10) {
+        return std::nullopt;
+    }
+
+    return parseUniqueEntry(metadata)
+        .flatMap([&](auto&& entry)
+                     -> utilxx::Opt<UniqueEntryOperation> {
+            switch(metadata[OPERATION_FLAG_INDEX]) {
+
+            case UNIQUE_ENTRY_CREATION_FLAG:
+                return UniqueEntryOperation{
+                    UniqueEntryCreationOp{std::move(entry),
+                                          std::move(owner),
+                                          block,
+                                          value}};
+
+            case UNIQUE_ENTRY_RENEWAL_FLAG:
+                return UniqueEntryOperation{
+                    UniqueEntryRenewalOp{std::move(entry),
+                                         std::move(owner),
+                                         block,
+                                         value}};
+
+            case UNIQUE_ENTRY_OWNERSHIP_TRANSFER_FLAG:
+                return new_owner_opt
+                    .map([&](auto&& new_owner) {
+                        return UniqueEntryOperation{
+                            UniqueEntryOwnershipTransferOp{std::move(entry),
+                                                           std::move(owner),
+                                                           std::move(new_owner),
+                                                           block,
+                                                           value}};
+                    });
+
+            case UNIQUE_ENTRY_DELETION_FLAG:
+                return UniqueEntryOperation{
+                    UniqueEntryDeletionOp{std::move(entry),
+                                          std::move(owner),
+                                          block,
+                                          value}};
+
+            default:
+                return std::nullopt;
+            }
+        });
+}
+
+auto forge::core::parseTransactionToUniqueEntry(Transaction&& tx,
+                                                std::int64_t block,
+                                                const daemon::ReadOnlyDaemonBase* daemon)
+    -> Result<Opt<UniqueEntryOperation>, DaemonError>
+{
+    using ResultType = Result<Opt<UniqueEntryOperation>, DaemonError>;
+
+    LOG_IF(FATAL, !daemon) << "ReadOnlyDaemonBase pointer is null";
+
+    //check if the transaction has exactly one op return
+    //output and exactly one input
+    //if this is not the case the tx does not repressent a forge op
+    if(!tx.hasExactlyOneOpReturnOutput()
+       || !tx.hasExactlyOneInput()) {
+        return ResultType{std::nullopt};
+    }
+
+    LOG(INFO) << tx.getTxid() << " contains a OP_RETURN output";
+
+    //save, because we checked that the tx has exactly one
+    //op return output
+    const auto& op_return_output =
+        tx.getFirstOpReturnOutput()
+            .getValue()
+            .get();
+
+    //get optional new owner
+    //for ownership transfer
+    auto new_owner_opt =
+        tx.getFirstNonOpReturnOutput()
+            .flatMap([](auto&& ref)
+                         -> Opt<std::string> {
+                //we only care about outputs with exactly one
+                //address
+                if(ref.get().getAddresses().size() != 1) {
+                    return std::nullopt;
+                }
+                return ref.get().getAddresses()[0];
+            });
+
+    //save, because we have checked that the tx has exactly
+    //one input
+    auto vin = std::move(tx.getInputs()[0]);
+
+    //value of the op return output
+    auto value = op_return_output.getValue();
+
+    //extract the metadata from the output script
+    auto output_script = std::move(op_return_output.getHex());
+    auto metadata_opt = extractMetadata(std::move(output_script));
+
+    if(!metadata_opt) {
+        return ResultType{std::nullopt};
+    }
+
+    //get metadata from the op return output
+    auto metadata = std::move(metadata_opt.getValue());
+
+    //check if the metadata starts with a forge id
+    //if not the tx is not a valid forge tx and can be ignored
+    if(!metadataStartsWithForgeId(metadata)) {
+        return ResultType{std::nullopt};
+    }
+
+    LOG(DEBUG) << "resoving vin from " << vin.getTxid();
+    return daemon
+        ->resolveTxIn(std::move(vin))
+        .flatMap([&](auto&& resolvedVin) {
+            //we can only have one input address
+            if(resolvedVin.getAddresses().size() != 1) {
+                return ResultType{std::nullopt};
+            }
+
+            //get owner
+            auto owner = std::move(resolvedVin.getAddresses()[0]);
+
+            //parse the metadata nd put it into
+            //the ResultType
+            return ResultType{
+                parseMetadata(std::move(metadata),
+                              block,
+                              std::move(owner),
+                              value,
+                              std::move(new_owner_opt))};
+        });
+}
+
+
+auto forge::core::toMetadata(const UniqueEntryOperation& op)
+    -> std::vector<std::byte>
+{
+    const auto& entry = getUniqueEntry(op);
+    auto flag = extractOperationFlag(op);
+
+    auto data = forge::core::uniqueEntryToRawData(entry);
+
+    data.insert(std::begin(data),
+                flag);
+
+    data.insert(std::begin(data),
+                UNIQUE_ENTRY_IDENTIFICATION_FLAG);
+
+    data.insert(std::begin(data),
+                std::begin(FORGE_IDENTIFIER_MASK),
+                std::end(FORGE_IDENTIFIER_MASK));
+
+    return data;
+}
